668fc8da68e0998ac94d9248a8a775fff657ce9f
KalaiGuru70 <kalaig442@gmail.com>
2026-06-10 18:27:39 +0530

    Day 6 - JS Variables practice

---
+++ js-practice.js
@@ -0,0 +1,20 @@
+//Using var,Old way,can change Reassign and Redecleration;
+
+var city="Coimbatore";
+console.log(city);
+
+//Using let but can change value or Reassign and cannot Redecleration;
+
+let age=35;
+console.log(age);
+
+age = 45;
+console.log(age);
+
+//Using Const but cannot change Redecleration or Reassign;
+
+const pi=3.14;
+console.log(pi);
+
+const name="kalai";
+console.log(name);
