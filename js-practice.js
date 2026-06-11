4246bf2164a8afbebf20ef55f982632dc734fdb7
KalaiGuru70 <kalaig442@gmail.com>
2026-06-11 18:29:24 +0530

    Day 6 - JS Condition (if/else)

--- js-practice.js
+++ js-practice.js
@@ -1,112 +1,42 @@
-// Operators
+//Condition if/else Statement
 
-//Arithmetic Operator 
+//Age check
 
-//Shop Bill calculate
-
-let price = 150;
-let quantity =4;
-let shipping = 50;
-let subtotal = price * quantity;
-let total = subtotal + shipping;
-
-console.log(`Subtotal: ${subtotal}`);   
-console.log(`Shipping: ${shipping}`);  
-console.log(`Total: ${total}`);  
-
-//Assignment Operators
-
-// Wallet money add/subtract
-let wallet = 1000;
-console.log(`Starting: ${wallet}`);
-
-wallet += 500;  // salary
-console.log(`After salary: ${wallet}`);
-
-wallet -= 200;  // grocery
-console.log(`After grocery: ${wallet}`); 
-
-wallet *= 2;    // doubled
-console.log(`Doubled: ${wallet}`);       
-
-wallet /= 4;    // split with friends
-console.log(`After split: ${wallet}`);   
-
-
-//Comparison Operator
-
-//Movie ticket age Check
-
-const UserAge = 16;
-const minAge = 18;
-
-console.log(UserAge >= minAge);    
-console.log(UserAge == "16");      
-console.log(UserAge === "16");     
-console.log(UserAge != minAge);    
-console.log(UserAge < minAge);     
-
-
-//Logical Operator
-
-// Job eligibility check
-
-const age = 22;
-const hasDegree = true;
-const hasExperience = false;
-
-// AND — both must be true
-if(age>=18 && hasDegree ){
-    console.log("Eligible");
+const Age = 22;
+if(Age >= 18){
+    console.log("Adult");
 }
 else{
-    console.log("Not Eligible");
+    console.log("Minor");
 }
 
-// OR One enough
+//Grade check
 
-if(hasDegree || hasExperience){
-    console.log("Can Apply");
+const mark = 85;
+if(mark >= 90){
+    console.log("A grade");
+}
+else if(mark >= 75){
+    console.log("B grade")
+}
+else if(mark >= 60){
+    console.log("C grade")
 }
 else{
-    console.log("Cannot Apply");
+    console.log("fail")
 }
 
-//Not 
-console.log(`Fresher: ${!hasExperience}`);
+//Temperature Check
 
+let temp= 60;
+if(temp > 35){
+    console.log("Hot");
+}
+else if(temp>=20 && temp<=35){
+    console.log("Normal");
+}
 
-//String Operator
-
-// Profile card
-const firstName = "Kalai";
-const lastName = "Selvi";
-const city = "Tiruppur";
-const job = "Frontend Developer";
-
-// Concatenation
-console.log(firstName + " " + lastName);
-
-// Template literal
-console.log(`Name: ${firstName} ${lastName}`);
-console.log(`City: ${city}`);
-console.log(`Job: ${job}`);
-console.log(`Profile: ${firstName} | ${city} | ${job}`);
-
-// Online store — free delivery check
-const orderAmount = 450;
-const delivery = orderAmount >= 500 ? "Free Delivery" : "Pay Rs.50 delivery";
-console.log(delivery); 
-
-// Exam result
-const marks = 72;
-const result = marks >= 50 ? "Pass " : "Fail ";
-console.log(result); 
-
-// Login check
-const isLoggedIn = true;
-const message = isLoggedIn ? "Welcome back!" : "Please login";
-console.log(message);
-
-
+else{
+    console.log("Cold");
+}
 
